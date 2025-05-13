c2e53037050ffa769740191827a8ed349b39aaaf
jysngmb <jaysongomba@gmail.com>
2025-05-14 07:59:34 +0800

    changes

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,4 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
-
-module.exports = {
-  publicPath: 'porfolio_g'
-}
+  transpileDependencies: true,
+});
